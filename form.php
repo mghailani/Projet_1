1bf23712499ada70b47755e2dd6e04725cce364b
GHAILANI Mohamed <mghailani@uae.ac.ma>
2024-11-18 19:19:28 +0100

    Agout du champ age au form.php

--- form.php
+++ form.php
@@ -10,6 +10,10 @@
             <td><input type="text" name="prenom" id=""></td>
         </tr>
         <tr>
+            <tr>
+                <td>Age</td>
+                <td><input type="number" name="Age" id=""></td>
+            </tr>
             <td><input type="submit" value="Envoyer"></td>
             <td><input type="reset" value="Annuler"></td>
         </tr>
