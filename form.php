ab79d10e8a6da4f25cf91b80a98d248313632202
GHAILANI Mohamed <mghailani@uae.ac.ma>
2024-11-18 18:22:50 +0100

    version 1 du form.php

---
+++ form.php
@@ -0,0 +1,19 @@
+<center>
+<form action="" method="post">
+    <table>
+        <tr>
+            <td>Nom</td>
+            <td><input type="text" name="nom" id=""></td>
+        </tr>
+        <tr>
+            <td>Prénom</td>
+            <td><input type="text" name="prenom" id=""></td>
+        </tr>
+        <tr>
+            <td><input type="submit" value="Envoyer"></td>
+            <td><input type="reset" value="Annuler"></td>
+        </tr>
+    </table>
+</form>
+
+</center>
